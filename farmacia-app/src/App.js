07c6474199ca6ca0001998f679e2c4114b567e57
Gabriel Apolinário <gab.apolinario@hotmail.com>
2024-04-08 22:06:07 -0300

    listando clientes na tela inicial com axios

--- farmacia-app/src/App.js
+++ farmacia-app/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Navbar from './components/Nav.js';
+import Table from './components/tables/Table.js';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Table/>
     </div>
   );
 }
--- farmacia-app/src/components/tables/Table.js
+++ farmacia-app/src/components/tables/Table.js
@@ -1,21 +1,41 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
 
-function Table(){
-    const [data, setData] = React.useState(null);
+function Table() {
+  const [listaClientes, setListaClientes] = React.useState(null);
 
-    React.useEffect(() => {
-    axios.get("http://localhost.com").then((response) => {
-        setData(response.data);
-    }); }, []);
+  React.useEffect(() => {
+    axios.get("http://localhost:8080/api/lista-cliente").then((response) => {
+      setListaClientes(response.data);
 
-    if(!data) return null;
-    
-    return(
-        <div>
-            <h1>{data.nome}</h1>
-        </div>
-    )
+      console.log(response);
+    });
+  }, []);
+
+  if (!listaClientes) return null;
+
+  return (
+    <table className="table table-light w-50 mx-auto">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>CPF CNPJ</th>
+        </tr>
+        {listaClientes.map((cliente) => {
+          return (
+            <tr>
+              <td>
+                <h3>{cliente.nome}</h3>
+              </td>
+              <td>
+                <h3>{cliente.cpf_cnpj}</h3>
+              </td>
+            </tr>
+          );
+        })}
+      </thead>
+    </table>
+  );
 }
 
 export default Table;
