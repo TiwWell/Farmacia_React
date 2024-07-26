ea54d1ed278f19b8438d83a58d016311cc503376
José Victor <jvictorfrance@outlook.com>
2024-07-25 21:06:42 -0300

    Corrigindo listagem dos remedios e desativado -> status

--- farmacia-app/src/components/Medicine/Medicine.js
+++ farmacia-app/src/components/Medicine/Medicine.js
@@ -5,139 +5,149 @@ import "./Medicine.css";
 import MedicineModal from "./MedicineModal";
 
 const Medicine = () => {
-    const [medicine, setMedicine] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [listaMedicines, setListaMedicines] = useState(null);
-    const [isAddMode, setIsAddMode] = useState(false);
+  const [medicine, setMedicine] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [listaMedicines, setListaMedicines] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
-    useEffect(() => {
-        handleSelect();
-    }, []);
+  useEffect(() => {
+    handleSelect();
+  }, []);
 
-    const handleSelect = () => {
-        try {
-            axios
-                .get("http://localhost:8080/api/listar-remedio")
-                .then((response) => {
-                    console.log(response);
-                    setListaMedicines(response.data);
-                })
-                .catch((error) => {
-                    console.error("Erro ao obter remedios:", error);
-                    toast.error(
-                        "Erro no carregamento da lista de remedios. Verificar se a API está disponível.",
-                        { position: "botton-right" }
-                    );
-                });
-        } catch (error) {
-            console.error("Erro ao configurar a solicitação:", error);
-            toast.error(
-                "Erro no carregamento da lista de remedios. Verificar se a API está disponível.",
-                { position: "botton-right" }
-            );
-        }
-    };
-
-    function handleDeactivate(medicineId) {
-        axios
-            .get(`http://localhost:8080/api/desativar-remedio/${medicineId}`)
-            .then((response) => {
-                toast.success("Desativado com sucesso");
-                window.location.reload();
-            });
+  const handleSelect = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/api/listar-remedio"
+      );
+      console.log(response.data.listaRemedios);
+      if (response.data.codRetorno === 200) {
+        setListaMedicines(response.data.listaRemedios);
+      } else {
+        toast.error(
+          "Erro no carregamento da lista de remédios. Verificar se a API está disponível.",
+          { position: "bottom-right" }
+        );
+      }
+    } catch (error) {
+      console.error("Erro ao configurar a solicitação:", error);
+      toast.error(
+        "Erro no carregamento da lista de remedios. Verificar se a API está disponível.",
+        { position: "botton-right" }
+      );
     }
+  };
 
-    function handleReactivate(medicineId) {
-        axios
-            .get(`http://localhost:8080/api/reativar-remedio/${medicineId}`)
-            .then((response) => {
-                toast.success("Reativado com sucesso");
-                window.location.reload();
-            });
+  const handleRevert = async (medicineId) => {
+    try {
+      await axios.get(
+        `http://localhost:8080/api/inverter-status-remedio/${medicineId}`
+      );
+      toast.success("Status do remedio invertido com sucesso");
+      handleSelect(); // Recarrega a lista de remédios após a inversão de status
+    } catch (error) {
+      console.error("Erro ao inverter status do remedio:", error);
+      toast.error(
+        "Erro ao inverter o status do remedio. Verificar se a API está disponível.",
+        { position: "bottom-right" }
+      );
     }
-    const openModal = (medicine, isAddMode) => {
-        setIsModalOpen(true);
-        setMedicine(medicine);
-        setIsAddMode(isAddMode);
-    };
+  };
 
-    const handleCallback = (childData) => {
-        setIsModalOpen(childData);
-    };
+  const openModal = (medicine, isAddMode) => {
+    setIsModalOpen(true);
+    setMedicine(medicine);
+    setIsAddMode(isAddMode);
+  };
 
-    if (!listaMedicines) return null;
-    return (
-        <>
-            <div>
-                <button
-                    type="submit"
-                    className="btn btn-primary float-end"
-                    onClick={() => openModal()}
-                >
-                    Adicionar
-                </button>
-            </div>
-            <div>
-                <table className="table table-light w-75 mx-auto">
-                    <thead>
-                        <tr className="text-center">
-                            <th>Imagem</th>
-                            <th>Nome</th>
-                            <th>Valor</th>
-                            <th>Quantidade</th>
-                            <th>Ações</th>
-                        </tr>
-                        {listaMedicines.map((medicine) => {
-                            return (
-                                <tr className="text-center" key={medicine.id}>
-                                    <td width={200}>
-                                        <img width={200} height={200} src={medicine.img}/>
-                                    </td>
-                                    <td width={200}>
-                                        <h4>{medicine.nome}</h4>
-                                    </td>
-                                    <td width={200}>
-                                        <h4>{medicine.valor}</h4>
-                                    </td>
+  const handleCallback = (childData) => {
+    setIsModalOpen(childData);
+  };
 
-                                    <td width={200}>
-                                        <h4>{medicine.quantidade}</h4>
-                                    </td>
-                                    <td width={200}>
-                                        <button
-                                            type="button"
-                                            className="btn btn-secondary me-1"
-                                            onClick={() => openModal(medicine, false)}
-                                        >
-                                            Alterar
-                                        </button>
-                                        {medicine.desativado === 0 ? (
-                                            <button type="submit" className="btn btn-danger" onClick={() => handleDeactivate(medicine.id)}>
-                                                Desativar
-                                            </button>
-                                        ) : (
-                                            <button type="submit" className="btn btn-success" onClick={() => handleReactivate(medicine.id)}>
-                                                Reativar
-                                            </button>
-                                        )}
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </thead>
-                </table>
-            </div>
-            {isModalOpen && (
-                <MedicineModal
-                    medicine={medicine}
-                    isModalOpen={isModalOpen}
-                    parentCallback={handleCallback}
-                    isAddMode={!medicine}
-                />
-            )}
-        </>
-    );
-
-}
+  if (!listaMedicines.length) return null;
+  return (
+    <>
+      <div>
+        <button
+          type="submit"
+          className="btn btn-primary float-end"
+          onClick={() => openModal(null, true)}
+        >
+          Adicionar
+        </button>
+      </div>
+      <div>
+        <table className="table table-light w-75 mx-auto">
+          <thead>
+            <tr className="text-center">
+              <th>Imagem</th>
+              <th>Nome</th>
+              <th>Valor</th>
+              <th>Quantidade</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listaMedicines.map((medicine) => (
+              <tr className="text-center" key={medicine.id}>
+                <td width={200}>
+                  <img
+                    width={200}
+                    height={200}
+                    src={medicine.img}
+                    alt={medicine.nome}
+                  />
+                </td>
+                <td width={200}>
+                  <h4>{medicine.nome}</h4>
+                </td>
+                <td width={200}>
+                  <h4>{medicine.valor}</h4>
+                </td>
+                <td width={200}>
+                  <h4>{medicine.quantidade}</h4>
+                </td>
+                <td width={200}>
+                  {/* Descomentar os botões quando necessário */}
+                  <button
+                    type="button"
+                    className="btn btn-secondary me-1"
+                    onClick={() => openModal(medicine, false)}
+                  >
+                    Alterar
+                  </button>
+                  {medicine.status === 0 ? (
+                    <button
+                      type="submit"
+                      className="btn btn-danger"
+                      onClick={() => handleRevert(medicine.id)}
+                    >
+                      Desativado
+                    </button>
+                  ) : (
+                    <button
+                      type="submit"
+                      className="btn btn-success"
+                      onClick={() => handleRevert(medicine.id)}
+                    >
+                      Ativado
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {isModalOpen && (
+        <MedicineModal
+          medicine={medicine}
+          isModalOpen={isModalOpen}
+          parentCallback={handleCallback}
+          isAddMode={!medicine}
+        />
+      )}
+    </>
+  );
+};
 
 export default Medicine;
