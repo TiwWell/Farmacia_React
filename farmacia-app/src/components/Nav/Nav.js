f76ef18ae8467607c1ac11f0972c53e126dcb834
TiwWell <wellissonrodrigues12@gmail.com>
2024-09-10 14:34:22 -0300

    Adicionado alert de desenvolvimento direto no nav.js.

--- farmacia-app/src/components/Nav/Nav.js
+++ farmacia-app/src/components/Nav/Nav.js
@@ -1,6 +1,6 @@
-import { Link, Outlet } from 'react-router-dom';
-import './Nav.css';
-import farmaciaNav from './farmacia logo.jpg';
+import { Link, Outlet } from "react-router-dom";
+import "./Nav.css";
+import farmaciaNav from "./farmacia logo.jpg";
 
 function Nav() {
   return (
@@ -9,23 +9,43 @@ function Nav() {
         <header>
           <nav className="navbar navbar-expand-lg custom-navbar">
             <div className="container-fluid">
-              <img src={farmaciaNav} alt="Descrição da imagem" className="nav-image" />
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <img
+                src={farmaciaNav}
+                alt="Descrição da imagem"
+                className="nav-image"
+              />
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav me-auto">
                   <li className="nav-item home-li">
-                    <Link className="nav-link" to="/home">Pagina Inicial</Link>
+                    <Link className="nav-link" to="/home">
+                      Pagina Inicial
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link className="nav-link" to="/clientes">Clientes</Link>
+                    <Link className="nav-link" to="/clientes">
+                      Clientes
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link className="nav-link" to="/remedios">Remédios</Link>
+                    <Link className="nav-link" to="/remedios">
+                      Remédios
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link className="nav-link" to="/farmaceuticos">Farmacêuticos</Link>
+                    <Link className="nav-link" to="/farmaceuticos">
+                      Farmacêuticos
+                    </Link>
                   </li>
                 </ul>
                 <ul className="navbar-nav">
@@ -34,6 +54,11 @@ function Nav() {
               </div>
             </div>
           </nav>
+          {/* Alerta informando que o site está em desenvolvimento */}
+          <div className="alert alert-warning" role="alert">
+            Este site é um protótipo em desenvolvimento e se trata de uma versão
+            inicial.
+          </div>
         </header>
       </div>
       <Outlet />
@@ -42,4 +67,3 @@ function Nav() {
 }
 
 export default Nav;
-  
